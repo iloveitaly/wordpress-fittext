4cc4fd85441706c22c32b49412635e7c7928a357
Michael Bianco <iloveitaly@gmail.com>
2014-02-07 13:29:13 -0500

    Initial commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+/*
+Plugin Name: FitText.js for WordPress
+Plugin URI: https://github.com/iloveitaly/wp-fitvids
+Description: Easily add fittext to your site
+Version: 1.0.0
+Author: Michael Bianco
+Author URI: http://cliffsidemedia.com/
+License: Public Domain
+*/
+
+if (!defined( 'ABSPATH' )) exit;
+
+define('FITTEXT_PLUGIN_URL', plugin_dir_url(  __FILE__  ) );
+
+add_action('wp_enqueue_scripts', 'fittext');
+function fittext() {
+	wp_enqueue_script('fitvids', FITTEXT_PLUGIN_URL . 'jquery.fittext.js', array('jquery'), '1.0');
+	wp_enqueue_script('fitvids.init', FITTEXT_PLUGIN_URL . 'jquery.fittext.init.js', array('jquery', 'fitvids'), '1.0');
+} ?>
---
+++ readme.md
@@ -0,0 +1,7 @@
+# FitText.js for WordPress
+
+A simple WordPress plugin that wraps up the [FitText.js](http://fittextjs.com) jQuery plugin.
+
+## How it works
+
+The plugin simply enqueues FitText.js and applies it to every element with the `fittext` class. Nice and simple.
